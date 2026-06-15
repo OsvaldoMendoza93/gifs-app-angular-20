1a8d24e02368f8647a997c1c15d340937a26b058
Osvaldo Mendoza <osvaldo.mendoza1249@gmail.com>
2026-06-15 15:10:55 -0600

    Feat: Se renombro el nombre del proyecto y limpieza de codigo

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, gifs-app-angular20');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, gifs-app-angular-20');
   });
 });
--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('gifs-app-angular20');
+  protected readonly title = signal('gifs-app-angular-20');
 }
